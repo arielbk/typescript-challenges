0a272bf145bd75c3eda88f407b63cd8610954451
arielbk <buchwald.kearney@gmail.com>
2022-07-31 15:37:43 +0200

    Another handful of easy challenges

---
+++ 00189-easy-awaited.ts
@@ -0,0 +1,25 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from './test-utils'
+
+type X = Promise<string>
+type Y = Promise<{ field: number }>
+type Z = Promise<Promise<string | number>>
+
+type cases = [
+  Expect<Equal<MyAwaited<X>, string>>,
+  Expect<Equal<MyAwaited<Y>, { field: number }>>,
+  Expect<Equal<MyAwaited<Z>, string | number>>,
+]
+
+// @ts-expect-error
+type error = MyAwaited<number>
+
+
+// ============= Your Code Here =============
+type MyAwaited<T extends Promise<any>> =
+  T extends Promise<infer U> 
+  // recursion until we get a non-promise
+    ? U extends Promise<any>
+      ? MyAwaited<U>
+      : U
+    : T;
