66e3069a4abf04d041977ea733ccb1ff64e9a6cc
arielbk <buchwald.kearney@gmail.com>
2022-08-05 21:37:48 +0200

    Medium challenges

--- 00016-medium-pop.ts
+++ 00016-medium-pop.ts
@@ -7,4 +7,17 @@ type cases = [
 ];
 
 // ============= Your Code Here =============
-type Pop<T extends any[]> = T extends [...infer Spread, any] ? Spread : never;
+type Pop<T extends any[]> = T extends [...infer Rest, any] ? Rest : never;
+
+type Shift<T extends any[]> = T extends [any, ...infer Rest] ? Rest : never;
+type ShiftTest = Shift<[3, 2, 1]>;
+
+type Push<T extends any[], P = never> = T extends [...infer Rest]
+  ? [...Rest, P]
+  : never;
+type PushTest = Push<[3, 2, 1], 0>;
+
+type Unshift<T extends any[], P = never> = T extends [...infer Rest]
+  ? [P, ...Rest]
+  : never;
+type UnshiftTest = Unshift<[3, 2, 1], 4>;
